0382f4c6c7235f0b9c39ed0d4d854852d7e7ff9b
안도현 <ahnjm001@playwith.co.kr>
2025-04-18 12:13:25 +0900

    feat: 예외 처리 추가, ObjectPool MakeShared 부분 알아보기 쉽게 수정
    이유
    1. 메모리 관리 명확성 -> 명시적으로 람다를 사용하여 커스텀 deleter를 정의
    2. 코드 가독성 향상

--- DhUtil/ObjectPool.h
+++ DhUtil/ObjectPool.h
@@ -17,8 +17,11 @@ public:
 	template<typename... Args>
 	static std::shared_ptr<Type> MakeShared(Args&&... _args)
 	{
-		std::shared_ptr<Type> sptr = { Pop(std::forward<Args>(_args)...), Push };
-		return sptr;
+		/*std::shared_ptr<Type> sptr = { Pop(std::forward<Args>(_args)...), Push };
+		return sptr;*/
+
+		Type* memory = Pop(std::forward<Args>(_args)...);
+		return std::shared_ptr<Type>(memory, [](Type* obj) { Push(obj); });
 	}
 
 private:
@@ -38,7 +41,6 @@ private:
 		{
 			//없으면 새로 만든다.
 			memory = new Type;
-
 			m_poolSize++;
 		}
 
@@ -52,7 +54,16 @@ private:
 		Type* memory = GetMemory();
 		ASSERT_CRASH(memory != nullptr);
 
-		new(memory)Type(std::forward<Args>(args)...);
+		try
+		{
+			new(memory)Type(std::forward<Args>(args)...);
+		}
+		catch (...)
+		{
+			// 초기화 실패 시 메모리 반환
+			Push(memory);
+			throw;
+		}
 
 #if _DEBUG
 		// std::cout << "ObjectPool::GetMemory() : " << typeid(Type).name() << " - poolSize : " << m_poolSize << std::endl;
@@ -64,6 +75,8 @@ private:
 
 	static void Push(Type* obj)
 	{
+		if (!obj) return;
+
 		obj->~Type();
 
 		{
@@ -76,6 +89,18 @@ private:
 #if _DEBUG
 		// std::cout << "ObjectPool::Push() : " << typeid(Type).name() << " - usedSize : " << m_usedSize << std::endl;
 #endif
+	}
+
+	static int GetPoolSize()
+	{
+		WriteLockGuard writeLockGuard(m_lock, typeid(ObjectPool<Type>).name());
+		return m_poolSize;
+	}
+
+	static int GetUsedSize()
+	{
+		WriteLockGuard writeLockGuard(m_lock, typeid(ObjectPool<Type>).name());
+		return m_usedSize;
 	}
 };
 
@@ -90,137 +115,3 @@ int ObjectPool<Type>::m_usedSize = 0;
 
 template <typename Type>
 int ObjectPool<Type>::m_poolSize = 0;
-
-/*#pragma once
-#include "pch.h"
-#include <mutex>
-
-template<typename T>
-class ObjectPool
-{
-private:
-	USE_LOCK;
-
-	std::queue<T*>	m_queue;
-
-	bool			m_isAlloc;
-	int				m_usedSize;
-	int				m_poolSize;
-
-public:
-	ObjectPool(int _size);
-	~ObjectPool();
-
-	void Init();
-	void Close();
-
-	T* NewObject();
-	bool DeleteObject(T* _object);
-
-	int GetMaxSize();
-	int GetUsedSize();
-};
-
-template<typename T>
-ObjectPool<T>::ObjectPool(int _size)
-{
-	m_isAlloc = false;
-	m_usedSize = 0;
-	m_poolSize = _size;
-}
-
-template<typename T>
-ObjectPool<T>::~ObjectPool()
-{
-	Close();
-}
-
-template<typename T>
-void ObjectPool<T>::Init()
-{
-	ASSERT_CRASH(m_isAlloc == false);
-
-	WRITE_LOCK;
-
-	for (int i = 0; i < m_poolSize; ++i)
-	{
-		try
-		{
-			T* tObject = new T;
-			m_queue.push(tObject);
-		}
-		catch (std::exception&)
-		{
-			assert(0 && "Init Failed");
-		}
-	}
-
-	m_usedSize = 0;
-	m_isAlloc = true;
-}
-
-template<typename T>
-void ObjectPool<T>::Close()
-{
-	WRITE_LOCK;
-
-	if (m_isAlloc == false) return;
-
-	while (m_queue.empty() == false)
-	{
-		m_queue.pop();
-	}
-
-	m_usedSize = 0;
-	m_isAlloc = false;
-}
-
-template<typename T>
-T* ObjectPool<T>::NewObject()
-{
-	ASSERT_CRASH(m_isAlloc == true);
-
-	T* object = nullptr;
-
-	{
-		WRITE_LOCK;
-
-		if (m_usedSize >= GetMaxSize()) return object;
-
-		object = m_queue.front();
-		m_queue.pop();
-		m_usedSize++;
-	}
-
-	ASSERT_CRASH(object != nullptr);
-
-	return object;
-}
-
-template<typename T>
-bool ObjectPool<T>::DeleteObject(T* _object)
-{
-	ASSERT_CRASH(m_isAlloc == true);
-	ASSERT_CRASH(_object != nullptr);
-
-	{
-		WRITE_LOCK;
-
-		m_queue.push(_object);
-		m_usedSize--;
-	}
-
-	return true;
-}
-
-template<typename T>
-int ObjectPool<T>::GetMaxSize()
-{
-	return m_poolSize;
-}
-
-template<typename T>
-int ObjectPool<T>::GetUsedSize()
-{
-	return m_usedSize;
-}*/
