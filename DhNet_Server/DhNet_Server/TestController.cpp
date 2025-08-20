250e75038802d76fdbd5cac8632633ff7c31d1aa
Dohyun Ahn <zeniel7303@users.noreply.github.com>
2025-08-20 12:24:55 +0900

    feat : Sender DeAlloc시 동기화 이슈 해결

--- DhNet_Server/DhNet_Server/TestController.cpp
+++ DhNet_Server/DhNet_Server/TestController.cpp
@@ -1,18 +1,18 @@
 #pragma once
 #include "stdafx.h"
 #include "TestController.h"
-#include "../ServerCore/Sender.h"
+#include "GameSession.h"
 
 bool HandleTestPacket(PacketHeader* _header, std::shared_ptr<Session>& _session)
 {
-	auto temp = reinterpret_cast<TestPacket*>(_header);
+	auto testPacket = reinterpret_cast<TestPacket*>(_header);
 
-	std::cout << "OnRecv Len " << temp->m_dataSize << " / " << temp->m_test << std::endl;
+	// std::cout << "OnRecv Len " << testPacket->m_dataSize << " / " << testPacket->m_test << std::endl;
 
-	auto sender = Sender::Alloc<TestPacket>();
-	auto packet = sender->GetWritePointer<TestPacket>();
-	packet->Init(PacketEnum::Test, sizeof(TestPacket));
-	strcpy_s(packet->m_test, "Test(Server)");
+	auto senderAndPacket = Sender::GetSenderAndPacket<TestPacket>();
+	senderAndPacket.first->Init(PacketEnum::Test, sizeof(TestPacket));
+	strcpy_s(senderAndPacket.first->m_test, "Test(Server)");
+	_session->Send(senderAndPacket.second);
 
 	return true;
 }
