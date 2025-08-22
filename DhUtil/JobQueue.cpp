0f19133f2cef60e82160bdadfe69206dd32046e8
안도현 <ahnjm001@playwith.co.kr>
2025-08-22 16:36:57 +0900

    feat : JobQueue의 2 이슈 수정
    1. 일감이 한번에, 그리고 계속 너무 몰리는 상황
    2. DoAsync 함수를 계속 타서(DoAsync해서 실행한 함수 안에 또 DoAsync가 있는 등...) 절대 끝나지 않는 상황 (일감이 한 쓰레드한테 몰리는 상황)
    
    1번 해결 : Execute에서 시간 체크를 통해 작업을 오래 점유하고 있으면 중단 후 여유있는 다른 쓰레드로 처리 위임 로직 추가
    2번 해결 : JobQueue에서 Push할 때 현재 JobQueue의 상태 파악 후 적절히 다른 쓰레드로 작업 위임 로직 추가

--- DhUtil/JobQueue.cpp
+++ DhUtil/JobQueue.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "JobQueue.h"
+#include "GlobalQueue.h"
 
 void JobQueue::Push(JobRef&& _job)
 {
@@ -10,16 +11,28 @@ void JobQueue::Push(JobRef&& _job)
 	// 첫번째 Job을 넣은 쓰레드가 실행까지 담당
 	if (prevCount == 0)
 	{
+	    if (LCurrentJobQueue != nullptr)
+	    {
+	    	// 이미 뭔가 실행 중인 JobQueue가 있으므로 여유 있는 다른 쓰레드에게 작업 위임
+	    	GGlobalQueue->Push(shared_from_this());
+			return;    
+	    }
+
+		// 이미 뭔가 실행 중인 JobQueue가 없으므로 직접 실행
 		Execute();
 	}
 }
 
 // TODO : 
 // 1. 일감이 한번에, 그리고 계속 너무 몰리는 상황
+//		-> Execute에서 시간 체크를 통해 해결
 // 2. DoAsync 함수를 계속 타서(DoAsync해서 실행한 함수 안에 또 DoAsync가 있는 등...) 
 //    절대 끝나지 않는 상황 (일감이 한 쓰레드한테 몰리는 상황)
+//		-> GGlobalQueue 분산 처리로 해결
 void JobQueue::Execute()
 {
+	LCurrentJobQueue = this;
+	
     while (true)
     {
         std::vector<JobRef> jobs;
@@ -42,10 +55,18 @@ void JobQueue::Execute()
         // 일감이 더 이상 없으면 중단
         if (m_jobCount.fetch_sub(jobCount) == jobCount)
         {
+        	LCurrentJobQueue = nullptr;
             return;
         }
 
-        // 처리 후 잠시 대기 (CPU 과부하 방지)
-        // std::this_thread::yield();
+    	const uint64 now = ::GetTickCount64();
+    	if (now >= LEndTickCount)
+    	{
+    		// 과도하게 한 쓰레드가 작업을 오래 점유하고 있으면 중단
+    		LCurrentJobQueue = nullptr;
+    		// 여유 있는 다른 쓰레드가 실행하도록 GlobalQueue에 넘긴다
+    		GGlobalQueue->Push(shared_from_this());
+    		break;
+    	}
     }
 }
