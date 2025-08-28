960bbbdba8a84940af4ecebc60b71f64180cffb2
안도현 <ahnjm001@playwith.co.kr>
2025-08-28 10:54:40 +0900

    feat : 비어있는 Room에 접속시 Thread-Safe하지 않은 부분 수정

--- DhNet_Server/DhNet_Server/Room.cpp
+++ DhNet_Server/DhNet_Server/Room.cpp
@@ -3,9 +3,19 @@
 
 void Room::Enter(std::shared_ptr<Player> _player)
 {
+	if (static_cast<int32>(m_players.size()) >= MAX_ROOM_PLAYER)
+	{
+		// 이 곳에 들어오기 전에 TryReserveSlot는 무조건 실행된다는 가정 하에 ReleaseReservedSlot 호출
+		ReleaseReservedSlot();
+		_player->EnterRoomFailed();
+		return;
+	}
+
 	auto [it, result] = m_players.try_emplace(_player->GetPlayerId(), _player);
 	if (!result)
 	{
+		// 이 곳에 들어오기 전에 TryReserveSlot는 무조건 실행된다는 가정 하에 ReleaseReservedSlot 호출
+		ReleaseReservedSlot();
 		_player->EnterRoomFailed();
 		return;
 	}
@@ -21,6 +31,7 @@ void Room::Leave(std::shared_ptr<Player> _player)
 	if (it != m_players.end())
 	{
 		m_players.erase(it);
+		m_availableSlots.fetch_add(1);
 	}
 }
 
@@ -30,4 +41,27 @@ void Room::Broadcast(std::shared_ptr<Sender> _sender)
 	{
 		p.second->GetOwnerSession()->Send(_sender);
 	}
+}
+
+bool Room::TryReserveSlot()
+{
+	while (true)
+	{
+		int32 avail = m_availableSlots.load();
+		if (avail <= 0)
+		{
+			return false;
+		}
+
+		// lock-free CAS loop
+		if (m_availableSlots.compare_exchange_weak(avail, avail - 1))
+		{
+			return true;
+		}
+	}
+}
+
+void Room::ReleaseReservedSlot()
+{
+	m_availableSlots.fetch_add(1);
 }
--- DhNet_Server/DhNet_Server/Room.h
+++ DhNet_Server/DhNet_Server/Room.h
@@ -8,13 +8,24 @@ class Room : public JobQueue
 private:
 	int32 m_roomIndex;
 	std::map<uint64, std::shared_ptr<Player>> m_players;
+	std::atomic<int32> m_availableSlots{ MAX_ROOM_PLAYER };
 
 public:
+	Room() : m_roomIndex(0) {}
+	~Room() = default;
+
 	void Enter(std::shared_ptr<Player> _player);
 	void Leave(std::shared_ptr<Player> _player);
 	void Broadcast(std::shared_ptr<Sender> _sender);
 
+	// Thread-safe 하도록 room 입장 예약을 거는 함수
+	bool TryReserveSlot();
+	void ReleaseReservedSlot();
+
 	int32 GetRoomIndex() const { return m_roomIndex; }
-	int32 GetPlayerCount() const { return static_cast<int32>(m_players.size()); }
+	// 외부에서도 안전하게 쓰일 수 있도록 수정 (but 예약 포함한 근사값을 반환함. 그래도 어지간하면 맞을 것)
+	int32 GetPlayerCount() const { return MAX_ROOM_PLAYER - m_availableSlots.load(); }
+	// int32 GetPlayerCount() const { return static_cast<int32>(m_players.size()); }
+	
 	void SetRoomIndex(int32 _roomIndex) { m_roomIndex = _roomIndex; }
 };
